c4cbd082df42dc5744c1726ec1b45e89ccbf825d
ricardo <elseoclub@gmail.com>
2024-11-25 23:31:37 -0600

    Add x-esc-layout on vendor:publish

--- src/ComponentsServiceProvider.php
+++ src/ComponentsServiceProvider.php
@@ -1,28 +1,40 @@
 <?php
 
+
 namespace Elseoclub\Components;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 
-class ComponentsServiceProvider extends ServiceProvider {
-    public function boot() {
-        $this->loadViewsFrom( __DIR__ . '/../resources/views', 'esc' );
+class ComponentsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'esc');
+
+        $this->publishes([
+            __DIR__.'/../resources/views/layouts/esc.blade.php' => resource_path('views/layouts/esc.blade.php'),
+        ], 'elseoclub-laravel-components');
+
+        $this->publishes([
+            __DIR__.'/../src/View/Components/EscLayout.stub' => app_path('View/Components/EscLayout.php'),
+        ], 'elseoclub-laravel-components');
 
         $this->registerBladeComponents();
     }
 
-    protected function registerBladeComponents() {
+    protected function registerBladeComponents()
+    {
         $componentNamespace = 'Elseoclub\\Components\\View\\Components';
-        $componentPath      = __DIR__ . '/View/Components';
+        $componentPath = __DIR__ . '/View/Components';
 
-        foreach ( scandir( $componentPath ) as $file ) {
-            if ( is_file( $componentPath . '/' . $file ) ) {
-                $componentClass = $componentNamespace . '\\' . pathinfo( $file, PATHINFO_FILENAME );
-                $componentAlias = Str::kebab( pathinfo( $file, PATHINFO_FILENAME ) );
+        foreach (scandir($componentPath) as $file) {
+            if (is_file($componentPath . '/' . $file)) {
+                $componentClass = $componentNamespace . '\\' . pathinfo($file, PATHINFO_FILENAME);
+                $componentAlias = Str::kebab(pathinfo($file, PATHINFO_FILENAME));
 
-                Blade::component( 'esc::components.' . $componentAlias, $componentClass );
+                Blade::component('esc::components.' . $componentAlias, $componentClass);
             }
         }
     }
